ba7237bae2f0a4309b47ac8f355b06f54dca40e0
shenhailong <819062373@qq.com>
2021-10-14 14:53:13 +0800

    d

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
+<!--
+ * @page: 页面-
+ * @Author: Dragon
+ * @Date: 2021-10-12 14:20:55
+ * @LastEditors: Dragon
+ * @LastEditTime: 2021-10-14 14:53:06
+-->
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Hom1e</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
